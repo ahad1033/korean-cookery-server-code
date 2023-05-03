e37d065f7ce00e013e38af190e5fb778d6c738cf
ahad1033 <a.ahad2232@gmail.com>
2023-05-04 01:18:09 +0600

    Fix line endings in package-lock.json and package.json

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('express is running')
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedCategories = categories.find(c => c._id === id);
+    res.send(selectedCategories)
+}) 
 
 app.listen(port, () => {
     console.log(`express is running on port: ${port}`)
